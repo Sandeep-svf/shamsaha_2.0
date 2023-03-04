66c25bb98c3fcca26df61787f180bcfe6301f132
Sandeep-svf <svf.sandeep@gmail.com>
2023-03-04 11:45:50 +0530

    sam

---
+++ app/src/main/java/com/sam/shamsaha/pickimagecameragalary/PickImagelocalActivity.kt
@@ -0,0 +1,15 @@
+package com.sam.shamsaha.pickimagecameragalary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sam.shamsaha.R
+
+class PickImagelocalActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pick_imagelocal)
+    }
+}
