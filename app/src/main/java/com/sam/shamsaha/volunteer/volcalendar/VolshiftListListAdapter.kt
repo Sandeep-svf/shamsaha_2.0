e3615a93f9afcea8c04944c6bfc47144f2e0662e
Sandeep-svf <svf.sandeep@gmail.com>
2023-03-27 12:58:59 +0530

    sam

--- app/src/main/java/com/sam/shamsaha/volunteer/volcalendar/VolshiftListListAdapter.kt
+++ app/src/main/java/com/sam/shamsaha/volunteer/volcalendar/VolshiftListListAdapter.kt
@@ -1,15 +1,20 @@
 package com.sam.shamsaha.volunteer.volcalendar
 
+import android.app.Dialog
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatTextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.sam.shamsaha.R
 
 
 import com.sam.shamsaha.databinding.VolShiftListViewHolderBinding
+import de.hdodenhof.circleimageview.CircleImageView
 
 class VolshiftListListAdapter(val context: Context, val valShiftList : List<volShiftListModel>) :
     RecyclerView.Adapter<VolshiftListListAdapter.ShiftListViewHolder>() {
@@ -40,9 +45,23 @@ class VolshiftListListAdapter(val context: Context, val valShiftList : List<volS
     }
 
     private fun popup_window() {
+        val dialog = Dialog(context)
 
-
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setCancelable(false)
+        dialog.setContentView(R.layout.popup_shift_registration)
+        val shift = dialog.findViewById(R.id.shift) as AppCompatTextView
+        //shift.text = "sam"
+        val accept_button = dialog.findViewById(R.id.accept_button) as AppCompatButton
+        val cancel_image_view = dialog.findViewById(R.id.cancel_image_view) as CircleImageView
+        accept_button.setOnClickListener {
+            dialog.dismiss()
         }
+        cancel_image_view.setOnClickListener {
+            dialog.dismiss()
+        }
+        dialog.show()
+    }
 
     override fun getItemCount(): Int {
       return valShiftList.size;
